51ce65b389dbd38e3eaa565efc92fb9858b855d4
ValenRoss <rodrossi25@gmail.com>
2024-12-22 21:51:18 -0300

    avance1

--- JS/main.js
+++ JS/main.js
@@ -14,8 +14,12 @@ const kit1 = new producto(1,"Kit Supervivencia 1",10000,"./assets/image1.png")
 const kit2 = new producto(2,"Kit Supervivencia 1",10000,"./assets/image1.png")
 const kit3 = new producto(3,"Kit Supervivencia 1",10000,"./assets/image1.png")
 const kit4 = new producto(4,"Kit Supervivencia 1",10000,"./assets/image1.png")
+const kit5 = new producto(5,"Kit Supervivencia 1",10000,"./assets/image1.png")
+const kit6 = new producto(6,"Kit Supervivencia 1",10000,"./assets/image1.png")
+const kit7 = new producto(7,"Kit Supervivencia 1",10000,"./assets/image1.png")
+const kit8 = new producto(8,"Kit Supervivencia 1",10000,"./assets/image1.png")
 
-const productosZ = [kit1,kit2,kit3,kit4];
+const productosZ = [kit1,kit2,kit3,kit4,kit5,kit6,kit7,kit8];
 console.log(productosZ) 
 
 const contenedorDeProductos = document.getElementById("contenedorDeProductos")
