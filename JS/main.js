5b0a91f96480d91f80f8830ca334298dd6d79297
ValenRoss <rodrossi25@gmail.com>
2024-12-28 20:16:39 -0300

    prefinal

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 
-let carrito = [];
+
 
 class producto {
     constructor(id,nombre,precio,img){
@@ -11,32 +11,48 @@ class producto {
     }
 }
 const kit1 = new producto(1,"Kit Supervivencia 1",10000,"./assets/image1.png")
-const kit2 = new producto(2,"Kit Supervivencia 1",10000,"./assets/image1.png")
-const kit3 = new producto(3,"Kit Supervivencia 1",10000,"./assets/image1.png")
-const kit4 = new producto(4,"Kit Supervivencia 1",10000,"./assets/image1.png")
-const kit5 = new producto(5,"Kit Supervivencia 1",10000,"./assets/image1.png")
-const kit6 = new producto(6,"Kit Supervivencia 1",10000,"./assets/image1.png")
-const kit7 = new producto(7,"Kit Supervivencia 1",10000,"./assets/image1.png")
-const kit8 = new producto(8,"Kit Supervivencia 1",10000,"./assets/image1.png")
+const kit2 = new producto(2,"Kit Supervivencia 2",10000,"./assets/image1.png")
+const kit3 = new producto(3,"Kit Supervivencia 3",10000,"./assets/image1.png")
+const kit4 = new producto(4,"Kit Supervivencia 4",10000,"./assets/image1.png")
+const kit5 = new producto(5,"Kit Supervivencia 5",10000,"./assets/image1.png")
+const kit6 = new producto(6,"Kit Supervivencia 6",10000,"./assets/image1.png")
+const kit7 = new producto(7,"Kit Supervivencia 7",10000,"./assets/image1.png")
+const kit8 = new producto(8,"Kit Supervivencia 8",10000,"./assets/image1.png")
 
-const productosZ = [kit1,kit2,kit3,kit4,kit5,kit6,kit7,kit8];
-console.log(productosZ) 
+const productos = [kit1,kit2,kit3,kit4,kit5,kit6,kit7,kit8];
+let carrito = [];
+console.log(productos) 
 
 const contenedorDeProductos = document.getElementById("contenedorDeProductos")
 
 const mostrarProductos = () => {
-    productosZ.forEach( producto => {
+    productos.forEach(producto => {
         const card = document.createElement("div")
         card.innerHTML =    `
                 <div id ="card">
-                    <img src ="${producto.img}" class = "cardImg">
+                    <img src ="${producto.img}" class = "cardImg"> 
                     <div class = "cardBody">
                     <h2> ${producto.nombre} </h2>
                     <p> $${producto.precio} </p>
-                    <button id= "boton">Agregar al Carrito</button>
+                    <button id= "boton${producto.id}">Agregar al Carrito</button>
                     </div>
                 </div>`
         contenedorDeProductos.appendChild(card)
+        const boton = document.getElementById(`boton${producto.id}`);
+        boton.addEventListener("click", () => {
+            agregarAlCarrito(productos.id);
+        })
     })
 }
-mostrarProductos()
+mostrarProductos()  
+
+const agregarAlCarrito = (id) => {
+    const productoEnCarrito = carrito.find(producto => producto.id === id);
+    if(productoEnCarrito){
+        productoEnCarrito.cantidad++;    
+    }else {
+        const producto = productos.find(producto => producto.id === id);
+        carrito.push(producto);
+    } 
+    console.log(carrito);
+}
